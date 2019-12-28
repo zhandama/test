4c7d8dcb0c8d927486dd814b0b03c41541f13a42
txm <>
2019-12-28 22:06:26 +0800

    修改文件

--- src/locale/index.js
+++ src/locale/index.js
@@ -2,11 +2,7 @@ import Vue from 'vue'
 import VueI18n from 'vue-i18n'
 import { localRead } from '@/libs/util'
 import customZhCn from './lang/zh-CN'
-import customZhTw from './lang/zh-TW'
-import customEnUs from './lang/en-US'
 import zhCnLocale from 'iview/src/locale/lang/zh-CN'
-import enUsLocale from 'iview/src/locale/lang/en-US'
-import zhTwLocale from 'iview/src/locale/lang/zh-TW'
 
 Vue.use(VueI18n)
 
@@ -20,9 +16,7 @@ Vue.config.lang = lang
 // vue-i18n 6.x+写法
 Vue.locale = () => {}
 const messages = {
-  'zh-CN': Object.assign(zhCnLocale, customZhCn),
-  'zh-TW': Object.assign(zhTwLocale, customZhTw),
-  'en-US': Object.assign(enUsLocale, customEnUs)
+  'zh-CN': Object.assign(zhCnLocale, customZhCn)
 }
 const i18n = new VueI18n({
   locale: lang,
